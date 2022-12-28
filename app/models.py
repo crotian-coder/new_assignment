3282030fd24979ad776f8d0b221f7a02c14c91f5
crotian-coder <crotiancoder@gmail.com>
2022-12-28 19:52:54 +0530

    added database integration for likes and dislikes counter

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	likes = models.IntegerField(default=0)
+	dislikes = models.IntegerField(default=0)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
+from .models import User
+from django.http import JsonResponse
+from django.http import QueryDict
 
 def home(request):
-	return render(request,'index.html')
+	if request.method == "GET":
+
+		context = {}
+		user = User.objects.all().last()
+
+		if user:
+			context["likes"] = user.likes 
+			context["dislikes"] = user.dislikes
+		else:
+			User.objects.create(likes=0,dislikes=0)
+
+		return render(request,'index.html',context=context)
+
+	elif request.method == "PUT":
+		put = QueryDict(request.body)
+		dislikes = put.get("dislikes")
+		likes = put.get("likes")
+
+		context = {}
+
+		user = User.objects.all().last()
+		if likes:
+			user.likes = user.likes + 1
+			context["likes"] = user.likes
+
+		if dislikes:
+			user.dislikes = user.dislikes + 1
+			context["dislikes"] = user.dislikes
+
+		user.save()
+
+		return JsonResponse(context)
+
+
+
